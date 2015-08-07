62069bb1ad932e5bf79e07cba33becbb34ccbfb5
Daniel-Claus-1314-fhb <clausd@fh-brandenburg.de>
2015-08-07 15:23:52 +0200

    add Testdata of csv export

--- SturzAppProject2/DataModel/MeasurementsList.cs
+++ SturzAppProject2/DataModel/MeasurementsList.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BackgroundTask.DataModel
 {
-    class MeasurementList
+    public class MeasurementList
     {
         private List<Measurement> _measurements;
         public List<Measurement> Measurements
